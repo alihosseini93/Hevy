f9d14d1b19d4a4b7210691ffbc12a91f37b0c187
unknown <salihosseini33@gmail.com>
2022-06-08 10:02:20 +0430

    add git

---
+++ src/Exercise.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Exercise() {
+    return(
+        <>
+            <h1>Ali</h1>
+        </>
+    )
+}
+
+export default Exercise;
---
+++ src/Routines.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Routines() {
+    return(
+        <>
+            <h1>Ali</h1>
+        </>
+    )
+}
+
+export default Routines;
